b461dac8f7ac39daae7a043520c0ecec0d0b9bfb
Lucien <lucien1011@gmail.com>
2015-11-29 14:14:37 +0100

    added script to print top news

---
+++ printNews.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+import urllib2
+from bs4 import BeautifulSoup
+
+#____________________________________________________________________________||
+
+topNewsDict = {'id':'top-news','class':'top-news'}
+firstColDict = {'class': 'first-column-region region'}
+headDict = {'class': 'story-heading'}
+summaryDict = {'class': 'summary'}
+
+#____________________________________________________________________________||
+
+url = "http://www.nytimes.com/"
+
+page = urllib2.urlopen( url )
+data = BeautifulSoup( page )
+
+#____________________________________________________________________________||
+
+topNews = data.body.find( 'section', attrs = topNewsDict ).find('article')
+
+#____________________________________________________________________________||
+
+print
+
+heading = topNews.find('h2', attrs = headDict )
+print "-"*len(heading.text)
+print heading.text
+print "-"*len(heading.text)
+
+#____________________________________________________________________________||
+
+summary = topNews.find('p', summaryDict )
+print summary.text
+
+print 
+#____________________________________________________________________________||
+
+#____________________________________________________________________________||
